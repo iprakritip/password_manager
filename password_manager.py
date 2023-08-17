612c17a51bae1632637a40c3395a54b4f70cc981
Prakriti Pokharel <iprakritip@gmail.com>
2023-08-17 21:30:35 +0545

    feat : create a new file to store data when first set of data is created

--- password_manager.py
+++ password_manager.py
@@ -9,16 +9,19 @@ def add():
     name = input("Account name: ")
     pwd=input("Password: ")
 
-
+    # open the password.txt file, append to it and close it. 
+    # a is a mode called append. others are w-write, r-read.
+    with open('passwords.txt', 'a') as f:
+        f.write(name + ' | '+ pwd + "\n")
 
 while True:
   mode= input("Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
   if mode == 'q':
     break
   elif mode=='view':
-    pass
+    view();
   elif mode=='add':
-    pass
+    add();
   else:
     print("Invalid mode.") 
     continue
